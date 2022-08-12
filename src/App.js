4576b6673b207883576a52afebb8f88519ce748f
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2022-08-12 01:35:13 -0700

    feat: add temp alert

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// temp - bootstrap
+import { Alert } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Alert style={{ textAlign: "center" }}>
+        This website is a work in progress, check out the codebase <a href="https://github.com/cubecademy/cubecademy.net">here</a>.
+      </Alert>
+      <h1>Cubecademy</h1>
+      <p>
+        We organize opportunities for curious minds to learn about Speedcubing.
+      </p>
+      <h2>View our social media profiles</h2>
+      <h2>Learn about our latest outreach</h2>
+      <h2>View our picture gallery</h2>
+      <h2>View our teaching cirriculum</h2>
+      <h2>Learn about our founding story</h2>
+    </>
   );
 }
 
